5801485b15d4f56c5530f429642f5787f232ebf8
Santosh T <bt.santosh01@gmail.com>
2022-09-30 20:05:30 +0100

    Setting up user authentication

---
+++ lib/movies_web/router.ex
@@ -0,0 +1,42 @@
+defmodule MoviesWeb.Router do
+  use MoviesWeb, :router
+  use Pow.Phoenix.Router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, {MoviesWeb.LayoutView, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/" do
+    pipe_through :browser
+
+    pow_routes()
+  end
+
+  scope "/", MoviesWeb do
+    pipe_through [:browser]
+
+    get "/", PageController, :index
+  end
+
+
+  # Enables the Swoosh mailbox preview in development.
+  #
+  # Note that preview only shows emails that were sent by the same
+  # node running the Phoenix server.
+  if Mix.env() == :dev do
+    scope "/dev" do
+      pipe_through :browser
+
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+end
